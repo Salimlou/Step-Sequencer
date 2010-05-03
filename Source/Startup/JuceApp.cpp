f612134af4e5ad9702666df580ab0a970679e2f5
Matt Sonic <matt@sonictransfer.com>
2010-05-02 20:58:04 -0700

    working example

---
+++ Source/Startup/JuceApp.cpp
@@ -0,0 +1,61 @@
+/*
+ *  JuceApp.cpp
+ *  template1
+ *
+ *  Created by Matt Sonic on 3/27/10.
+ *  Copyright 2010 SonicTransfer. All rights reserved.
+ *
+ */
+
+#include "juce_StandaloneFilterWindow.h"
+
+#include "JuceApp.h"
+
+JuceApp::JuceApp() :
+standaloneFilterWindow (0)
+{
+}
+
+JuceApp::~JuceApp()
+{
+}
+
+void JuceApp::initialise (const String& commandLine)
+{
+	const String applicationName = "PluginTemplate1";
+	const String fileNameSuffix("app");
+	const String folderName("SonicTransfer");
+	ApplicationProperties::getInstance()->setStorageParameters(applicationName,
+															   fileNameSuffix,
+															   folderName,
+															   0,
+															   PropertiesFile::storeAsXML);
+	
+	standaloneFilterWindow = new StandaloneFilterWindow ("PluginTemplate1", 
+														 Colours::lightgrey);
+	standaloneFilterWindow->setVisible (true);
+}
+
+void JuceApp::shutdown()
+{
+	if (standaloneFilterWindow != 0) delete standaloneFilterWindow;
+}
+
+const String JuceApp::getApplicationName()
+{
+	return T("PluginTemplate1");
+}
+
+const String JuceApp::getApplicationVersion()
+{
+	return T("1.0");
+}
+
+bool JuceApp::moreThanOneInstanceAllowed()
+{
+	return true;
+}
+
+void JuceApp::anotherInstanceStarted (const String& commandLine)
+{
+}
